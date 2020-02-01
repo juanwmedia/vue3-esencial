650dba851257d6f88a0de642939199e6de380dad
Juan <juan@wmedia.,es>
2020-02-01 11:28:25 +0100

    Search refactor

--- src/components/AppTaskSearch.vue
+++ src/components/AppTaskSearch.vue
@@ -3,8 +3,6 @@
     <h2>Search tasks</h2>
     <hr />
     <input
-      type="search"
-      :value="search"
       @input="input($event.target.value)"
       placeholder="Start searching..."
     />
@@ -14,7 +12,6 @@
 <script>
 export default {
   name: "AppTaskSearch",
-  props: ["search"],
   methods: {
     input(value) {
       this.$emit("input", value);
