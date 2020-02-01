f7ecfc59e2db4108a25c51fcc7bed1a6b5cf6c94
Juan <juan@wmedia.,es>
2020-02-01 11:13:51 +0100

    Custom search method

--- src/components/AppTaskSearch.vue
+++ src/components/AppTaskSearch.vue
@@ -5,7 +5,7 @@
     <input
       type="search"
       :value="search"
-      @input="$emit('input', $event.target.value)"
+      @input="input($event.target.value)"
       placeholder="Start searching..."
     />
   </div>
@@ -14,6 +14,11 @@
 <script>
 export default {
   name: "AppTaskSearch",
-  props: ["search"]
+  props: ["search"],
+  methods: {
+    input(value) {
+      this.$emit("input", value);
+    }
+  }
 };
 </script>
